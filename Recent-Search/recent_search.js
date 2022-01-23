43682dd049bc6fa5b0e2e4c04c14664872d79790
ProbisMis <buraksmeric@gmail.com>
2022-01-23 15:27:30 +0300

    first commit

---
+++ Recent-Search/recent_search.js
@@ -0,0 +1,35 @@
+// Search for Tweets within the past seven days
+// https://developer.twitter.com/en/docs/twitter-api/tweets/search/quick-start/recent-search
+
+import Needle from 'needle';
+import Dotenv from 'dotenv';
+
+Dotenv.config();
+
+const token = process.env.TWITTER_ACCESS_TOKEN;
+
+const endpointUrl = "https://api.twitter.com/2/tweets/search/recent";
+
+export async function getRequest(highestTweetId) {
+
+    const params = {
+        'query': '#altin OR #Altin',
+        'since_id' : highestTweetId,
+        'max_results': 15,
+        'tweet.fields': 'author_id,conversation_id,created_at,geo,id,lang,source,text'
+    }
+
+    const res = await Needle('get', endpointUrl, params, {
+        headers: {
+            "User-Agent": "v2RecentSearchJS",
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (res.body) {
+        return res.body;
+    } else {
+        throw new Error('Unsuccessful request');
+    }
+}
+
---
+++ index.js
@@ -0,0 +1,50 @@
+//import { getRequest } from './Tweet-Lookup/get_tweets_with_bearer_token.js';
+import { getRequest } from './Recent-Search/recent_search.js';
+import NodeCache  from 'node-cache';
+import fs from 'fs';
+
+
+//Retrieve highest id searched last time OK
+//Search for the tweets OK
+//Validate list OK 
+
+//Find the parent tweet 
+
+//Comment on parent tweet 
+
+//Deployment and automation
+
+function getRecentTweets(cb)
+{
+    getHighestTweetId(async function(data) {
+        console.log(data);
+        const response = await getRequest(data);
+        console.log(response); 
+        if (response.meta.result_count == 0)
+           return false;
+        writeToFile(response.meta.newest_id);
+        cb(response);
+       }); 
+}
+
+function getHighestTweetId(cb)
+{
+    fs.readFile('helloworld.txt', 'utf8', function (err,data) {
+        if (err) {
+          return console.log(err);
+        }
+        cb(data);
+
+      }); 
+}
+
+
+function writeToFile(highestTweetId)
+{
+    fs.writeFile('helloworld.txt', highestTweetId, function (err) {
+        if (err) return console.log(err);
+        console.log('Hello World > helloworld.txt');
+      });
+}
+
+
